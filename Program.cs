bb41ca28a2af3e1fc8a4b96b349d784159325744
Justin Prim <justinprim140969@gmail.com>
2024-01-10 14:52:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+            SayHiUser("Justin");
+            SayHiAge("Justin", 10); 
+            Console.ReadLine(); 
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("hi "); 
+        }
+
+        static void SayHiUser(string name)
+        {
+            Console.WriteLine("hi "+name);
+
+        }
+
+        static void SayHiAge(string name, int age)
+        {
+            Console.WriteLine("hi " + name+ " you are "+ age);
+        }
+    }
+}
